6ac6d065653a90b28ae6df7017e92ca666993ec1
Dmytro Tur <dmytro_tur@epam.com>
2018-11-23 12:35:18 +0200

    Adding comments

--- src/main/java/org/folio/rest/exception/UnprocessableEntityMessage.java
+++ src/main/java/org/folio/rest/exception/UnprocessableEntityMessage.java
@@ -2,11 +2,20 @@ package org.folio.rest.exception;
 
 import java.io.Serializable;
 
+/**
+ * Model describing reason of {@link UnprocessableEntityException}
+ */
 public class UnprocessableEntityMessage implements Serializable {
 
   private String code;
   private String message;
 
+  /**
+   * Constructor
+   *
+   * @param code    error code
+   * @param message human readable message
+   */
   public UnprocessableEntityMessage(String code, String message) {
     this.code = code;
     this.message = message;
