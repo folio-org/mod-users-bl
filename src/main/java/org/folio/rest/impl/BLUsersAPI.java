ed7d3ba4c92b55319526bc4534dcfea853e2e6d6
Adesh_Singh <Adesh_Singh@epam.com>
2024-02-23 15:02:24 +0530

    [MODUSERSBL-183-test]-Test fix

--- src/main/java/org/folio/rest/impl/BLUsersAPI.java
+++ src/main/java/org/folio/rest/impl/BLUsersAPI.java
@@ -982,29 +982,38 @@ public class BLUsersAPI implements BlUsers {
             Response permsResponse = cf.get();
             handleResponse(permsResponse, false, true, false, aRequestHasFailed, asyncResultHandler);
             if(!aRequestHasFailed[0] && permsResponse.getBody() != null){
-//              //data coming in from the service isnt returned as required by the composite user schema
-//              JsonObject j = new JsonObject();
-//              j.put("permissions", permsResponse.getBody().getJsonArray("permissionNames"));
+              //data coming in from the service isnt returned as required by the composite user schema
+              JsonObject j = new JsonObject();
+              j.put("permissions", permsResponse.getBody().getJsonArray("permissionNames"));
+              List<Permission> permissions1 = new ArrayList<>();
 //              cu.setPermissions((Permissions) Response.convertToPojo(j, Permissions.class));
+              Permissions permissions = (Permissions) Response.convertToPojo(j, Permissions.class);
+              permissions.getPermissions().parallelStream().forEach(Permission -> {
+                permissions1.add(Permission);
+                //cu.setPermissions( permissions);
+              });
+              permissions.setPermissions(permissions1);
+              cu.setPermissions(permissions);
 
-              logger.info("Yes try it....");
-              // Solution 1 -
-              JsonArray jsonArray = permsResponse.getBody().getJsonArray("permissionNames");
-
-              jsonArray.stream()
-                .filter(Permissions.class::isInstance)
-                .map(Permissions.class::cast)
-                .forEach(permissionName -> {
-                  JsonObject j = new JsonObject();
-                  j.put("permissions", Json.createArrayBuilder().add((JsonValue) permissionName).build());
-                  try {
-                    cu.setPermissions((Permissions) Response.convertToPojo(j, Permissions.class));
-                  } catch (Exception e) {
-                    throw new RuntimeException(e);
-                  }
-                });
-
-logger.info("YES did it....");
+//              logger.info("Yes try it....");
+//              // Solution 1 -
+//              JsonArray jsonArray = permsResponse.getBody().getJsonArray("permissionNames");
+//
+//              jsonArray.stream()
+//                .filter(Permissions.class::isInstance)
+//                .map(Permissions.class::cast)
+//                .forEach(permissionName -> {
+//                  JsonObject j = new JsonObject();
+//                  j.put("permissions", Json.createArrayBuilder().add((JsonValue) permissionName).build());
+//                  try {
+//                    cu.setPermissions((Permissions) Response.convertToPojo(j, Permissions.class));
+//                  } catch (Exception e) {
+//                    logger.info("Caught it..........");
+//                    throw new RuntimeException(e);
+//                  }
+//                });
+//
+//logger.info("YES did it....");
             }
           }
           cf = completedLookup.get(PERMISSIONS_INCLUDE);
