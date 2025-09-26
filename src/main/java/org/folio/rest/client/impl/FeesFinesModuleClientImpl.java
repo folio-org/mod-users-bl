981351fa7e525e87f07b76c66df6bd675cc73628
kapil-epam <kapil_verma1@epam.com>
2025-09-26 15:10:37 +0530

    MODUSERBL-231: removed StringUtil.urlEncode

--- src/main/java/org/folio/rest/client/impl/FeesFinesModuleClientImpl.java
+++ src/main/java/org/folio/rest/client/impl/FeesFinesModuleClientImpl.java
@@ -5,10 +5,13 @@ import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpMethod;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hc.core5.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.rest.client.FeesFinesModuleClient;
 import org.folio.rest.exception.OkapiModuleClientException;
 import org.folio.rest.util.OkapiConnectionParams;
 import org.folio.rest.util.RestUtil;
+import org.folio.service.PasswordResetLinkServiceImpl;
 import org.folio.util.PercentCodec;
 import org.folio.util.StringUtil;
 
@@ -16,6 +19,8 @@ import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
 public class FeesFinesModuleClientImpl implements FeesFinesModuleClient {
+
+  private static final Logger LOG = LogManager.getLogger(FeesFinesModuleClientImpl.class);
 
   private HttpClient httpClient;
 
@@ -45,7 +50,8 @@ public class FeesFinesModuleClientImpl implements FeesFinesModuleClient {
 
   @Override
   public Future<Integer> getNonExpiredManualBlocksCountByUserId(String userId, OkapiConnectionParams connectionParams) {
-    String query = StringUtil.urlEncode("(userId==" + StringUtil.cqlEncode(userId) + " AND expirationDate>=" + OffsetDateTime.now(ZoneOffset.UTC) + ")");
+    String query = "(userId==" + StringUtil.cqlEncode(userId) + " AND expirationDate>=" + OffsetDateTime.now(ZoneOffset.UTC) + ")";
+    LOG.info("getNonExpiredManualBlocksCountByUserId:: Looking up non-expired manual blocks of user with query: {}, percentEncoded: {}", query, PercentCodec.encode(query));
     String requestUrl = connectionParams.getOkapiUrl() + "/manualblocks?limit=0&query=" + PercentCodec.encode(query);
     return RestUtil.doRequest(httpClient, requestUrl, HttpMethod.GET,
       connectionParams.buildHeaders(), StringUtils.EMPTY)
